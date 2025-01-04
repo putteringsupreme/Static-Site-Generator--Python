938d00a607d869113639d95d6951da74f3b26dc3
putteringsupreme <seanronchetti@gmail.com>
2025-01-04 16:18:03 -0600

    Added basic file structure and defined text Enums

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import http
+from textnode import TextType, TextNode
+
+def main():
+    Testerson = TextNode("Testing TextNode", TextType.BOLD, "this is a url")
+    print(Testerson)
+    
+
+main()
